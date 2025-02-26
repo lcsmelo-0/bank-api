47337d79cac15731fef2ea9279be55920f1ea24b
Lucas de Melo Lima <luucasmell@gmail.com>
2025-02-26 19:56:06 -0300

    feat/ adicionar endpoint de login com dados mockados

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  login(@Body() body: { username: string; password: string }) {
+    const { username, password } = body;
+    if (this.authService.validateUser(username, password)) {
+      return { status: 'Login successful' };
+    }
+
+    throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private readonly users = [{ username: 'admin', password: 'admin' }];
+
+  validateUser(username: string, password: string): boolean {
+    return this.users.some(
+      (user) => user.username === username && user.password === password,
+    );
+  }
+}
