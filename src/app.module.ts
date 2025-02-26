9124967c73e25cd527349452e9a467f89c01e7f0
Lucas de Melo Lima <luucasmell@gmail.com>
2025-02-26 19:59:40 -0300

    chore/ adicionar estrutura inicial do modulo de accounts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+
+import { AccountsModule } from './accounts/accounts.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, AccountsModule],
   controllers: [AppController],
   providers: [AppService],
 })
